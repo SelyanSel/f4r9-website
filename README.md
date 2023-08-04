65fab6961e2b67f0d58d99b83d0ff8d13f4a8962
Alazard Célian <celian.alazard@gmail.com>
2023-08-04 02:00:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# f4r9 website
+ The source code for my website (https://f4r9.com/)
---
+++ index.js
@@ -0,0 +1,11 @@
+function contactBtnClicked(){
+    window.location = "mailto:celian.alazard@outlook.fr"
+}
+
+function openLink(link){
+    window.location = link
+}
+
+function github(){
+    window.location = "https://github.com/SelyanSel"
+}
